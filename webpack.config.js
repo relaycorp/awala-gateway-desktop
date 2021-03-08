d1764ae1d54926cdf5b0a07360b1ecfa8f86abee
lilia <liliakai@gmail.com>
2021-03-08 22:38:51 +0100

    Run tsc before webpack
    On second thought, just run tsc first.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const glob = require('glob')
 
 module.exports = [
   {
@@ -37,36 +36,6 @@ module.exports = [
         template: './src/electron/index.html'
       })
     ]
-  },
-  {
-    mode: 'development',
-    entry: toObject(glob.sync('./src/**/*.spec.ts')),
-    target: 'node',
-    module: {
-      rules: [{
-        test: /\.ts$/,
-        include: /src/,
-        use: [{ loader: 'ts-loader' }]
-      }]
-    },
-    resolve: {
-      extensions: ['.js', '.jsx', '.ts', '.tsx'],
-    },
-    output: {
-      path: __dirname + '/build/main',
-      filename: '[name].js'
-    }
   }
 ];
 
-function toObject(paths) {
-  var ret = {};
-
-  paths.forEach(function(path) {
-    // you can define entry names mapped to [name] here
-    let filename = path.match(/\.\/src\/(.*)\.ts/)[1];
-    ret[filename] = path;
-  });
-
-  return ret;
-}
