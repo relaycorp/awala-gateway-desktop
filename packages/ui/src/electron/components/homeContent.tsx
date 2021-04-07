fff9a18341f7ced925da3215e75c95e3115d2cb3
lilia <liliakai@gmail.com>
2021-04-07 19:07:11 +0200

    Add content and layout styles for home

--- packages/ui/src/electron/components/homeContent.tsx
+++ packages/ui/src/electron/components/homeContent.tsx
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 interface HomeContentProps {
   readonly title: string,
   readonly image: string,
+  readonly className?: string
 }
 class HomeContent extends Component<HomeContentProps> {
   public render() : JSX.Element {
@@ -10,7 +11,9 @@ class HomeContent extends Component<HomeContentProps> {
       <div className="home">
         <img src={this.props.image} />
         <h1>{this.props.title}</h1>
-        <div className='content'> {this.props.children} </div>
+        <div className={'content ' + this.props.className}>
+          {this.props.children}
+        </div>
       </div>
     );
   }
