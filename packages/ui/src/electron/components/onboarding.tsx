e91ebf356c463ba20cce29ac303e69b8c007283f
lilia <liliakai@gmail.com>
2021-04-07 18:31:43 +0200

    Fill in onboarding content

--- packages/ui/src/electron/components/onboarding.tsx
+++ packages/ui/src/electron/components/onboarding.tsx
@@ -39,11 +39,44 @@ class Onboarding extends Component<Props, State> {
   private getContent(step: number) : JSX.Element {
     switch (step) {
       case OnboardingStep.WELCOME:
-        return <h1>Welcome to Awala</h1>;
+      return (
+        <div>
+          <h1>Welcome to Awala</h1>
+          <p>
+            Awala enables compatible apps on your device to send and receive data
+            from the Internet securely, even when you’re disconnected from the
+            Internet.
+          </p>
+        </div>
+      );
       case OnboardingStep.EASY:
-        return <h1>Using Awala is easy</h1>;
+      return (
+        <div>
+          <h1>Using Awala is easy</h1>
+          <p>
+            When you’re connected to the Internet, your apps will simply use
+            it seamlessly.
+          </p>
+          <p>
+            When you’re cut off from the Internet, Awala couriers will
+            transport your apps’ data to and from the Internet.
+          </p>
+        </div>
+      );
       case OnboardingStep.START:
-        return <h1>Awala keeps you safe</h1>;
+      return (
+        <div>
+        <h1>Awala keeps you safe</h1>
+          <p>
+            Awala apps use end-to-end encryption, so it’s impossible for this
+            app or couriers to see their data.
+          </p>
+          <p>
+            It’s also impossible for couriers to find out which apps you’re
+            using. And needless to say we aren’t tracking you or your apps!
+          </p>
+        </div>
+      );
       default:
         return <p>Unknown step</p>
     }
