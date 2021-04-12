57599012ddaa7098456d7bc6f785ed9f497afe0d
Gus Narea <gnarea@users.noreply.github.com>
2021-04-12 15:58:45 +0100

    feat(daemon): Implement endpoint to retrieve current gateway address

--- packages/daemon/src/server/control/publicGateway.spec.ts
+++ packages/daemon/src/server/control/publicGateway.spec.ts
@@ -1,14 +1,21 @@
 import { PublicAddressingError } from '@relaycorp/relaynet-core';
 import LightMyRequest from 'light-my-request';
+import { Container } from 'typedi';
 
+import { Config } from '../../Config';
+import { DEFAULT_PUBLIC_GATEWAY } from '../../constants';
 import { GatewayRegistrar } from '../../sync/publicGateway/GatewayRegistrar';
 import { NonExistingAddressError } from '../../sync/publicGateway/gscClient';
+import { makeConfigTokenEphemeral } from '../../testUtils/config';
 import { setUpTestDBConnection } from '../../testUtils/db';
 import { getMockInstance, mockSpy } from '../../testUtils/jest';
 import { makeMockLogging, MockLogging, partialPinoLog } from '../../testUtils/logging';
+import { PUBLIC_GATEWAY_ADDRESS } from '../../tokens';
 import { makeServer } from '../index';
 
 setUpTestDBConnection();
+
+makeConfigTokenEphemeral(PUBLIC_GATEWAY_ADDRESS);
 
 let mockLogging: MockLogging;
 beforeEach(() => {
@@ -17,7 +24,30 @@ beforeEach(() => {
 
 const mockRegister = mockSpy(jest.spyOn(GatewayRegistrar.prototype, 'register'));
 
-const PUBLIC_ADDRESS = 'example.com';
+const NEW_PUBLIC_ADDRESS = `new.${DEFAULT_PUBLIC_GATEWAY}`;
+const ENDPOINT_PATH = '/_control/public-gateway';
+
+describe('Get public gateway', () => {
+  test('The current gateway should be returned if registered', async () => {
+    const config = Container.get(Config);
+    await config.set(PUBLIC_GATEWAY_ADDRESS, NEW_PUBLIC_ADDRESS);
+    const fastify = await makeServer(mockLogging.logger);
+
+    const response = await fastify.inject({ method: 'GET', url: ENDPOINT_PATH });
+
+    expect(response.statusCode).toEqual(200);
+    expect(JSON.parse(response.body)).toHaveProperty('publicAddress', NEW_PUBLIC_ADDRESS);
+  });
+
+  test('The default gateway should be returned if unregistered', async () => {
+    const fastify = await makeServer(mockLogging.logger);
+
+    const response = await fastify.inject({ method: 'GET', url: ENDPOINT_PATH });
+
+    expect(response.statusCode).toEqual(200);
+    expect(JSON.parse(response.body)).toHaveProperty('publicAddress', DEFAULT_PUBLIC_GATEWAY);
+  });
+});
 
 describe('Set public gateway', () => {
   test('Request should be refused if content type is not JSON', async () => {
@@ -26,8 +56,8 @@ describe('Set public gateway', () => {
     const response = await fastify.inject({
       headers: { 'content-type': 'text/plain' },
       method: 'PUT',
-      payload: { publicAddress: PUBLIC_ADDRESS },
-      url: '/_control/public-gateway',
+      payload: { publicAddress: NEW_PUBLIC_ADDRESS },
+      url: ENDPOINT_PATH,
     });
 
     expect(response.statusCode).toEqual(415);
@@ -59,14 +89,14 @@ describe('Set public gateway', () => {
     const error = new NonExistingAddressError('Address does not exist');
     getMockInstance(mockRegister).mockRejectedValue(error);
 
-    const response = await requestPublicAddressChange(PUBLIC_ADDRESS);
+    const response = await requestPublicAddressChange(NEW_PUBLIC_ADDRESS);
 
-    expect(mockRegister).toBeCalledWith(PUBLIC_ADDRESS);
+    expect(mockRegister).toBeCalledWith(NEW_PUBLIC_ADDRESS);
     expect(response.statusCode).toEqual(400);
     expect(JSON.parse(response.body)).toHaveProperty('code', 'INVALID_ADDRESS');
     expect(mockLogging.logs).toContainEqual(
       partialPinoLog('warn', 'Public address does not exist', {
-        publicAddress: PUBLIC_ADDRESS,
+        publicAddress: NEW_PUBLIC_ADDRESS,
       }),
     );
   });
@@ -75,15 +105,15 @@ describe('Set public gateway', () => {
     const error = new PublicAddressingError('Could not resolve address');
     getMockInstance(mockRegister).mockRejectedValue(error);
 
-    const response = await requestPublicAddressChange(PUBLIC_ADDRESS);
+    const response = await requestPublicAddressChange(NEW_PUBLIC_ADDRESS);
 
-    expect(mockRegister).toBeCalledWith(PUBLIC_ADDRESS);
+    expect(mockRegister).toBeCalledWith(NEW_PUBLIC_ADDRESS);
     expect(response.statusCode).toEqual(500);
     expect(JSON.parse(response.body)).toHaveProperty('code', 'ADDRESS_RESOLUTION_FAILURE');
     expect(mockLogging.logs).toContainEqual(
       partialPinoLog('warn', 'Failed to resolve public address', {
         err: expect.objectContaining({ type: error.name, message: error.message }),
-        publicAddress: PUBLIC_ADDRESS,
+        publicAddress: NEW_PUBLIC_ADDRESS,
       }),
     );
   });
@@ -92,41 +122,41 @@ describe('Set public gateway', () => {
     const error = new Error('Registration failed');
     getMockInstance(mockRegister).mockRejectedValue(error);
 
-    const response = await requestPublicAddressChange(PUBLIC_ADDRESS);
+    const response = await requestPublicAddressChange(NEW_PUBLIC_ADDRESS);
 
-    expect(mockRegister).toBeCalledWith(PUBLIC_ADDRESS);
+    expect(mockRegister).toBeCalledWith(NEW_PUBLIC_ADDRESS);
     expect(response.statusCode).toEqual(500);
     expect(JSON.parse(response.body)).toHaveProperty('code', 'REGISTRATION_FAILURE');
     expect(mockLogging.logs).toContainEqual(
       partialPinoLog('warn', 'Failed to complete registration', {
         err: expect.objectContaining({ message: error.message }),
-        publicAddress: PUBLIC_ADDRESS,
+        publicAddress: NEW_PUBLIC_ADDRESS,
       }),
     );
   });
 
   test('Change should be accepted if registration succeeds', async () => {
-    const response = await requestPublicAddressChange(PUBLIC_ADDRESS);
+    const response = await requestPublicAddressChange(NEW_PUBLIC_ADDRESS);
 
-    expect(mockRegister).toBeCalledWith(PUBLIC_ADDRESS);
+    expect(mockRegister).toBeCalledWith(NEW_PUBLIC_ADDRESS);
     expect(response.statusCode).toEqual(204);
     expect(mockLogging.logs).toContainEqual(
       partialPinoLog('info', 'Gateway migration completed', {
-        publicAddress: PUBLIC_ADDRESS,
+        publicAddress: NEW_PUBLIC_ADDRESS,
       }),
     );
   });
 
   test('Trailing dots should be removed from final address', async () => {
-    const addressWithTrailingDot = `${PUBLIC_ADDRESS}.`;
+    const addressWithTrailingDot = `${NEW_PUBLIC_ADDRESS}.`;
 
     const response = await requestPublicAddressChange(addressWithTrailingDot);
 
-    expect(mockRegister).toBeCalledWith(PUBLIC_ADDRESS);
+    expect(mockRegister).toBeCalledWith(NEW_PUBLIC_ADDRESS);
     expect(response.statusCode).toEqual(204);
     expect(mockLogging.logs).toContainEqual(
       partialPinoLog('info', 'Gateway migration completed', {
-        publicAddress: PUBLIC_ADDRESS,
+        publicAddress: NEW_PUBLIC_ADDRESS,
       }),
     );
   });
@@ -139,7 +169,7 @@ describe('Set public gateway', () => {
       headers: { 'content-type': 'application/json' },
       method: 'PUT',
       payload: { publicAddress },
-      url: '/_control/public-gateway',
+      url: ENDPOINT_PATH,
     });
   }
 });
