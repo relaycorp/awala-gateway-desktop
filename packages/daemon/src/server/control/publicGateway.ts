04dcb5559492f59186d7cd25c2d908aeb1657f77
Gus Narea <gnarea@users.noreply.github.com>
2021-04-12 11:12:17 +0100

    implement tests

---
+++ packages/daemon/src/server/control/publicGateway.ts
@@ -0,0 +1,10 @@
+import { FastifyInstance } from 'fastify';
+
+import RouteOptions from '../RouteOptions';
+
+export default async function registerRoutes(
+  _fastify: FastifyInstance,
+  _options: RouteOptions,
+): Promise<void> {
+  // TODO
+}
