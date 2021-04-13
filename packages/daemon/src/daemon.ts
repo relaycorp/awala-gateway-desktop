7adf7607281fd452d0d09cbb243da098fc97f070
Gus Narea <gnarea@users.noreply.github.com>
2021-04-13 22:36:43 +0100

    fix(daemon): Make TypeORM work with transpiled code (#78)

--- packages/daemon/src/daemon.ts
+++ packages/daemon/src/daemon.ts
@@ -1,10 +1,22 @@
-import { createConnection } from 'typeorm';
+import { createConnection, getConnectionOptions } from 'typeorm';
 
 import { makeServer, runServer } from './server';
 import { makeLogger } from './utils/logging';
 
+const IS_TYPESCRIPT = __filename.endsWith('.ts');
+
 export default async function (): Promise<void> {
-  await createConnection();
+  await createDBConnection();
   const server = await makeServer(makeLogger());
   await runServer(server);
+}
+
+async function createDBConnection(): Promise<void> {
+  const originalConnectionOptions = await getConnectionOptions();
+  /* istanbul ignore next */
+  const connectionOptions = {
+    ...originalConnectionOptions,
+    ...(!IS_TYPESCRIPT && { entities: ['build/entity/**/*.js'] }),
+  };
+  await createConnection(connectionOptions);
 }
