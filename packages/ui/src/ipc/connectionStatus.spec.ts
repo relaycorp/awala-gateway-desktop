0e3b431a0d9e7e57a3f23d7bd9bf88f94cb83027
lilia <liliakai@gmail.com>
2021-04-14 19:07:05 +0200

    Update connectionStatus tests
    Mocks the connect method from it-ws.

--- packages/ui/src/ipc/connectionStatus.spec.ts
+++ packages/ui/src/ipc/connectionStatus.spec.ts
@@ -1,10 +1,31 @@
 import pipe from 'it-pipe';
+import * as itws from 'it-ws';
+import { sleep } from './_utils';
 
 import { asyncIterableToArray, iterableTake } from '../testUtils/iterables';
 import { ConnectionStatus, pollConnectionStatus } from './connectionStatus';
 
+jest.mock('it-ws', () => ({
+  connect: jest.fn(),
+}));
+
 describe('pollConnectionStatus', () => {
-  test('should temporarily cycle through all the possible statuses', async () => {
+  test('load the status over a websocket', async () => {
+    (itws.connect as jest.Mock).mockReturnValue({
+      source: (async function* fakeSource(): AsyncIterable<string> {
+        yield 'CONNECTED_TO_PUBLIC_GATEWAY';
+        await sleep(1);
+
+        yield 'CONNECTED_TO_COURIER';
+        await sleep(1);
+
+        yield 'DISCONNECTED_FROM_PUBLIC_GATEWAY';
+        await sleep(1);
+
+        yield 'DISCONNECTED_FROM_ALL';
+      })(),
+    });
+
     jest.setTimeout(30_000);
 
     const statuses = await pipe(
