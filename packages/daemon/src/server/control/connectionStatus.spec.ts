eadbe21b162195fec4d7317215f07970274a2dd9
Gus Narea <gnarea@users.noreply.github.com>
2021-04-19 16:55:05 +0100

    fix connectionStatus.spec.ts

--- packages/daemon/src/server/control/connectionStatus.spec.ts
+++ packages/daemon/src/server/control/connectionStatus.spec.ts
@@ -2,10 +2,15 @@ import { MockClient } from '@relaycorp/ws-mock';
 import { Container } from 'typedi';
 
 import { ConnectionStatus, StatusMonitor } from '../../sync/StatusMonitor';
+import { useTemporaryAppDirs } from '../../testUtils/appDirs';
 import { restoreStatusMonitor } from '../../testUtils/connectionStatus';
+import { setUpTestDBConnection } from '../../testUtils/db';
 import { makeMockLogging, MockLogging } from '../../testUtils/logging';
 import { mockWebsocketStream } from '../../testUtils/websocket';
 import makeConnectionStatusServer from './connectionStatus';
+
+setUpTestDBConnection();
+useTemporaryAppDirs();
 
 mockWebsocketStream();
 restoreStatusMonitor();
@@ -16,6 +21,11 @@ beforeEach(() => {
 });
 
 describe('Stream', () => {
+  beforeEach(async () => {
+    const statusMonitor = Container.get(StatusMonitor);
+    statusMonitor.setLastStatus(ConnectionStatus.DISCONNECTED);
+  });
+
   test('Status changes should be streamed', async () => {
     const server = makeConnectionStatusServer(mockLogging.logger);
     const client = new MockClient(server);
