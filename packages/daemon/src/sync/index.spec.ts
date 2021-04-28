0b1333982a3fe4c28850587df23af33de4d64219
Gus Narea <gnarea@users.noreply.github.com>
2021-04-28 12:05:19 +0100

    register if necessary

--- packages/daemon/src/sync/index.spec.ts
+++ packages/daemon/src/sync/index.spec.ts
@@ -2,12 +2,20 @@ import { useTemporaryAppDirs } from '../testUtils/appDirs';
 import { setUpTestDBConnection } from '../testUtils/db';
 import { mockSpy } from '../testUtils/jest';
 import runSync from './index';
+import { GatewayRegistrar } from './publicGateway/GatewayRegistrar';
 import { StatusMonitor } from './StatusMonitor';
 
 setUpTestDBConnection();
 useTemporaryAppDirs();
 
 const mockStatusMonitorStart = mockSpy(jest.spyOn(StatusMonitor.prototype, 'start'));
+
+const mockGatewayRegistrarConditionalRegistration = mockSpy(
+  jest.spyOn(GatewayRegistrar.prototype, 'registerIfUnregistered'),
+  () => {
+    // Do nothing
+  },
+);
 
 describe('runSync', () => {
   test('Status monitor should be started', async () => {
@@ -16,5 +24,11 @@ describe('runSync', () => {
     await runSync();
 
     expect(mockStatusMonitorStart).toBeCalled();
+  });
+
+  test('Gateway should be registered if necessary', async () => {
+    await runSync();
+
+    expect(mockGatewayRegistrarConditionalRegistration).toBeCalled();
   });
 });
--- packages/daemon/src/sync/index.ts
+++ packages/daemon/src/sync/index.ts
@@ -1,9 +1,11 @@
 import { Container } from 'typedi';
 
+import { GatewayRegistrar } from './publicGateway/GatewayRegistrar';
 import { StatusMonitor } from './StatusMonitor';
 
 export default async function runSync(): Promise<void> {
   const statusMonitor = Container.get(StatusMonitor);
+  const gatewayRegistrar = Container.get(GatewayRegistrar);
 
-  await statusMonitor.start();
+  await Promise.all([statusMonitor.start(), gatewayRegistrar.registerIfUnregistered()]);
 }
