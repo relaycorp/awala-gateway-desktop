694455d1605c2c7840f2f565c479d6888de11926
lilia <liliakai@gmail.com>
2021-04-29 18:36:02 +0200

    lint

--- packages/ui/src/ipc/message.spec.ts
+++ packages/ui/src/ipc/message.spec.ts
@@ -2,13 +2,13 @@ import { ServerMessage, ServerMessageType } from '../ipc/message';
 
 describe('ServerMessage', () => {
   test('is defined', async () => {
-    function handler(message: ServerMessage) {
+    function handler(message: ServerMessage): ServerMessage {
       return message;
     }
-    const message = handler({
+    const serverMessage = handler({
       type: ServerMessageType.TOKEN_MESSAGE,
       value: 'authtoken',
     });
-    expect(message.value).toEqual('authtoken');
+    expect(serverMessage.value).toEqual('authtoken');
   });
 });
