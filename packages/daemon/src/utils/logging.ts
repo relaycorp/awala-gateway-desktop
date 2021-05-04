bac1a37371fb475180e442fe2c2718affaf8189f
Gus Narea <gnarea@users.noreply.github.com>
2021-05-04 18:29:12 +0100

    undo changes to daemon

--- packages/daemon/src/utils/logging.ts
+++ packages/daemon/src/utils/logging.ts
@@ -1,23 +1,7 @@
-import { mkdirSync } from 'fs';
-import path from 'path';
-
-import envPaths from 'env-paths';
 import { get as getEnvVar } from 'env-var';
 import pino, { Level, Logger } from 'pino';
 
 export function makeLogger(): Logger {
-  const paths = envPaths('AwalaGateway', { suffix: '' });
-  mkdirSync(paths.log, { recursive: true });
-
   const logLevel = getEnvVar('LOG_LEVEL').default('info').asString().toLowerCase() as Level;
-  return pino(
-    {
-      level: logLevel,
-      prettyPrint: {
-        colorize: false,
-        translateTime: 'yyyy-dd-mm, h:MM:ss TT',
-      },
-    },
-    pino.destination(path.join(paths.log, 'awala-daemon.log')),
-  );
+  return pino({ level: logLevel });
 }
