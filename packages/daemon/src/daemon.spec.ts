605869f2a96c521521b184bb3faab6cfaa843a05
Gus Narea <gnarea@users.noreply.github.com>
2021-05-05 11:27:23 +0100

    create dirs

--- packages/daemon/src/daemon.spec.ts
+++ packages/daemon/src/daemon.spec.ts
@@ -1,3 +1,4 @@
+import { promises as fs } from 'fs';
 import { join } from 'path';
 import { Container } from 'typedi';
 import * as typeorm from 'typeorm';
@@ -26,6 +27,10 @@ beforeEach(() => {
 });
 mockSpy(jest.spyOn(logging, 'makeLogger'), () => mockLogging.logger);
 
+const mockMkdir = mockSpy(jest.spyOn(fs, 'mkdir'));
+
+const PATHS = envPaths('AwalaGateway', { suffix: '' });
+
 test('Server should be run', async () => {
   await daemon();
 
@@ -38,7 +43,35 @@ test('Sync should be run', async () => {
   expect(runSync).toBeCalled();
 });
 
+describe('App directories', () => {
+  test('Data directory should be created', async () => {
+    await daemon();
+
+    expect(mockMkdir).toBeCalledWith(PATHS.data, { recursive: true });
+  });
+
+  test('Log directory should be created', async () => {
+    await daemon();
+
+    expect(mockMkdir).toBeCalledWith(PATHS.log, { recursive: true });
+  });
+
+  test('APP_DIRS token should be registered', async () => {
+    expect(Container.has(APP_DIRS)).toBeFalse();
+
+    await daemon();
+
+    expect(Container.get(APP_DIRS)).toEqual(PATHS);
+  });
+});
+
 describe('Logging', () => {
+  test('Logger factory should receive path to log directory', async () => {
+    await daemon();
+
+    expect(logging.makeLogger).toBeCalledWith(PATHS.log);
+  });
+
   test('Logger should be enabled by default', async () => {
     await daemon();
 
@@ -54,27 +87,15 @@ describe('Logging', () => {
   });
 });
 
-describe('App directories', () => {
-  test('APP_DIRS token should be registered', async () => {
-    expect(Container.has(APP_DIRS)).toBeFalse();
-
-    await daemon();
-
-    const expectedPaths = envPaths('AwalaGateway', { suffix: '' });
-    expect(Container.get(APP_DIRS)).toEqual(expectedPaths);
-  });
-});
-
 test('DB connection should be established', async () => {
   const originalConnectionOptions = await typeorm.getConnectionOptions();
 
   await daemon();
 
-  const expectedPaths = envPaths('AwalaGateway', { suffix: '' });
   const entitiesDir = __filename.endsWith('.ts')
     ? join(__dirname, 'entity', '**', '*.ts')
     : join(__dirname, 'entity', '**', '*.js');
-  const dbPath = join(expectedPaths.data, 'db.sqlite');
+  const dbPath = join(PATHS.data, 'db.sqlite');
   expect(mockCreateConnection).toBeCalledWith({
     ...originalConnectionOptions,
     database: dbPath,
--- packages/daemon/src/utils/logging.ts
+++ packages/daemon/src/utils/logging.ts
@@ -1,7 +1,7 @@
 import { get as getEnvVar } from 'env-var';
 import pino, { Level, Logger } from 'pino';
 
-export function makeLogger(): Logger {
+export function makeLogger(_logDir?: string): Logger {
   const logLevel = getEnvVar('LOG_LEVEL').default('info').asString().toLowerCase() as Level;
   return pino({ level: logLevel });
 }
