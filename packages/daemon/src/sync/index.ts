c11c60b261a73c882dc25cdbed25a6636fbb599b
Gus Narea <gnarea@users.noreply.github.com>
2021-06-23 15:31:46 +0100

    start subprocess at startup, after registration completes

--- packages/daemon/src/sync/index.ts
+++ packages/daemon/src/sync/index.ts
@@ -1,17 +1,24 @@
 import { Container } from 'typedi';
 
 import { GatewayRegistrar } from './publicGateway/GatewayRegistrar';
+import { ParcelCollectorManager } from './publicGateway/parcelCollection/ParcelCollectorManager';
 import { ParcelDeliveryManager } from './publicGateway/parcelDelivery/ParcelDeliveryManager';
 import { StatusMonitor } from './StatusMonitor';
 
 export default async function runSync(): Promise<void> {
   const statusMonitor = Container.get(StatusMonitor);
-  const gatewayRegistrar = Container.get(GatewayRegistrar);
-  const parcelDeliveryManager = Container.get(ParcelDeliveryManager);
 
+  await Promise.all([statusMonitor.start(), startSubprocesses()]);
+}
+
+async function startSubprocesses(): Promise<void> {
+  const gatewayRegistrar = Container.get(GatewayRegistrar);
+  await gatewayRegistrar.waitForRegistration();
+
+  const parcelDeliveryManager = Container.get(ParcelDeliveryManager);
+  const parcelCollectorManager = Container.get(ParcelCollectorManager);
   await Promise.all([
-    statusMonitor.start(),
+    parcelCollectorManager.start(),
     parcelDeliveryManager.deliverWhileConnected(),
-    gatewayRegistrar.registerIfUnregistered(),
   ]);
 }
