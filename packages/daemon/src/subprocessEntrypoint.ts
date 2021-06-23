bf7eed0be4e2f05c87886a5c14629ec5720d2ac0
Gus Narea <gnarea@users.noreply.github.com>
2021-06-23 18:08:33 +0100

    register parcel-collection subprocess

--- packages/daemon/src/subprocessEntrypoint.ts
+++ packages/daemon/src/subprocessEntrypoint.ts
@@ -2,11 +2,13 @@ import { Duplex } from 'stream';
 import { Container } from 'typedi';
 
 import runStartup from './startup';
+import runParcelCollection from './sync/publicGateway/parcelCollection/subprocess';
 import runParcelDelivery from './sync/publicGateway/parcelDelivery/subprocess';
 import { LOGGER } from './tokens';
 import { makeParentStream } from './utils/subprocess/parent';
 
 const SUBPROCESSES: { readonly [key: string]: (s: Duplex) => Promise<number> } = {
+  'parcel-collection': runParcelCollection,
   'parcel-delivery': runParcelDelivery,
 };
 
