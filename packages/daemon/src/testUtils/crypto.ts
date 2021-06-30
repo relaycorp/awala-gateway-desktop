fb09f87b0c938a82c83225d43d0da5b16ce2f9ed
Gus Narea <gnarea@users.noreply.github.com>
2021-06-30 21:34:14 +0100

    reimpl mockGatewayRegistration() to do the real thing

--- packages/daemon/src/testUtils/crypto.ts
+++ packages/daemon/src/testUtils/crypto.ts
@@ -1,3 +1,4 @@
+import { PrivateKey } from '@relaycorp/keystore-db';
 import {
   CDACertPath,
   generateCDACertificationPath,
@@ -7,12 +8,15 @@ import {
   PDACertPath,
 } from '@relaycorp/relaynet-testing';
 import { BinaryLike, createHash, Hash } from 'crypto';
+import { Container } from 'typedi';
+import { getRepository } from 'typeorm';
 
+import { Config, ConfigKey } from '../Config';
 import { DEFAULT_PUBLIC_GATEWAY } from '../constants';
-import { UnregisteredGatewayError } from '../errors';
+import { ConfigItem } from '../entity/ConfigItem';
+import { FileStore } from '../fileStore';
 import { DBPrivateKeyStore } from '../keystores/DBPrivateKeyStore';
-import { GatewayRegistrar } from '../sync/publicGateway/GatewayRegistrar';
-import { mockSpy } from './jest';
+import { PUBLIC_GATEWAY_ID_CERTIFICATE_OBJECT_KEY } from '../sync/publicGateway/GatewayRegistrar';
 
 function makeSHA256Hash(plaintext: BinaryLike): Hash {
   return createHash('sha256').update(plaintext);
@@ -50,54 +54,49 @@ export function generatePKIFixture(
   });
 }
 
-export function mockGatewayRegistration(pkiFixtureRetriever: PKIFixtureRetriever): () => void {
-  const mockGetPublicGateway = mockSpy(
-    jest.spyOn(GatewayRegistrar.prototype, 'getPublicGateway'),
-    () => {
-      const { pdaCertPath } = pkiFixtureRetriever();
-      return {
-        identityCertificate: pdaCertPath.publicGateway,
-        publicAddress: DEFAULT_PUBLIC_GATEWAY,
-      };
-    },
-  );
-  const mockIsRegistered = mockSpy(
-    jest.spyOn(GatewayRegistrar.prototype, 'isRegistered'),
-    () => true,
-  );
+export function mockGatewayRegistration(
+  pkiFixtureRetriever: PKIFixtureRetriever,
+): () => Promise<void> {
+  beforeEach(async () => {
+    const { cdaCertPath, pdaCertPath, keyPairSet } = pkiFixtureRetriever();
 
-  const mockGetCurrentKey = mockSpy(
-    jest.spyOn(DBPrivateKeyStore.prototype, 'getCurrentKey'),
-    () => {
-      const { pdaCertPath, keyPairSet } = pkiFixtureRetriever();
-      return {
-        certificate: pdaCertPath.privateGateway,
-        privateKey: keyPairSet.privateGateway.privateKey,
-      };
-    },
-  );
-  const mockGetOrCreateCCAIssuer = mockSpy(
-    jest.spyOn(DBPrivateKeyStore.prototype, 'getOrCreateCCAIssuer'),
-    () => {
-      const { cdaCertPath } = pkiFixtureRetriever();
-      return cdaCertPath.privateGateway;
-    },
-  );
-  const mockFetchNodeCertificates = mockSpy(
-    jest.spyOn(DBPrivateKeyStore.prototype, 'fetchNodeCertificates'),
-    () => {
-      const { pdaCertPath } = pkiFixtureRetriever();
-      return [pdaCertPath.privateGateway];
-    },
-  );
+    const privateKeyStore = Container.get(DBPrivateKeyStore);
+    const config = Container.get(Config);
+    const fileStore = Container.get(FileStore);
 
-  return () => {
-    mockGetPublicGateway.mockResolvedValue(null);
-    mockIsRegistered.mockResolvedValue(false);
-    mockGetCurrentKey.mockResolvedValue(null);
-    mockFetchNodeCertificates.mockResolvedValue([]);
-    mockGetOrCreateCCAIssuer.mockRejectedValue(
-      new UnregisteredGatewayError('Mocking gateway deregistration'),
+    await privateKeyStore.saveNodeKey(
+      keyPairSet.privateGateway.privateKey,
+      pdaCertPath.privateGateway,
     );
+    await config.set(
+      ConfigKey.NODE_KEY_SERIAL_NUMBER,
+      pdaCertPath.privateGateway.getSerialNumberHex(),
+    );
+
+    await config.set(ConfigKey.PUBLIC_GATEWAY_ADDRESS, DEFAULT_PUBLIC_GATEWAY);
+    await fileStore.putObject(
+      Buffer.from(pdaCertPath.publicGateway.serialize()),
+      PUBLIC_GATEWAY_ID_CERTIFICATE_OBJECT_KEY,
+    );
+
+    await privateKeyStore.saveNodeKey(
+      keyPairSet.privateGateway.privateKey,
+      cdaCertPath.privateGateway,
+    );
+    await config.set(
+      ConfigKey.NODE_CCA_ISSUER_KEY_SERIAL_NUMBER,
+      cdaCertPath.privateGateway.getSerialNumberHex(),
+    );
+  });
+
+  return async () => {
+    const configItemRepo = getRepository(ConfigItem);
+    await configItemRepo.clear();
+
+    const privateKeyRepo = getRepository(PrivateKey);
+    await privateKeyRepo.clear();
+
+    const fileStore = Container.get(FileStore);
+    await fileStore.deleteObject(PUBLIC_GATEWAY_ID_CERTIFICATE_OBJECT_KEY);
   };
 }
