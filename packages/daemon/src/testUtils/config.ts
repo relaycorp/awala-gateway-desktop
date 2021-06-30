d1a35647727a88bec76f760e2b8824fcf421485f
Gus Narea <gnarea@users.noreply.github.com>
2021-06-30 21:34:26 +0100

    remove unused file

--- packages/daemon/src/testUtils/config.ts
+++
@@ -1,9 +0,0 @@
-import { Container, Token } from 'typedi';
-
-export function makeConfigTokenEphemeral(configToken: Token<string>): void {
-  const removeToken = () => {
-    Container.remove(configToken);
-  };
-  beforeEach(removeToken);
-  afterAll(removeToken);
-}
