ed9482249a3442827251767da3470b4226d6a9b1
Gus Narea <gnarea@users.noreply.github.com>
2021-07-28 21:22:25 +0100

    implement

--- packages/daemon/src/server/control/courierSync.spec.ts
+++ packages/daemon/src/server/control/courierSync.spec.ts
@@ -30,12 +30,10 @@ test('Stages should be streamed', async () => {
   const server = makeCourierSyncServer(AUTH_TOKEN);
   const client = new MockAuthClient(server, AUTH_TOKEN);
 
-  await client.connect();
-
-  await expect(client.receive()).resolves.toEqual(CourierSyncStage.COLLECTION);
-  await expect(client.receive()).resolves.toEqual(CourierSyncStage.WAIT);
-
-  client.close();
+  await client.use(async () => {
+    await expect(client.receive()).resolves.toEqual(CourierSyncStage.COLLECTION);
+    await expect(client.receive()).resolves.toEqual(CourierSyncStage.WAIT);
+  });
 });
 
 test('Connection should be closed with 1000 when sync completes normally', async () => {
@@ -43,9 +41,9 @@ test('Connection should be closed with 1000 when sync completes normally', async
   const server = makeCourierSyncServer(AUTH_TOKEN);
   const client = new MockAuthClient(server, AUTH_TOKEN);
 
-  await client.connect();
-
-  await expect(client.waitForPeerClosure()).resolves.toEqual({ code: 1000 });
+  await client.use(async () => {
+    await expect(client.waitForPeerClosure()).resolves.toEqual({ code: 1000 });
+  });
 });
 
 test('Connection should be closed with 4000 when gateway is not yet registered', async () => {
@@ -55,12 +53,13 @@ test('Connection should be closed with 4000 when gateway is not yet registered',
   const server = makeCourierSyncServer(AUTH_TOKEN);
   const client = new MockAuthClient(server, AUTH_TOKEN);
 
-  await client.connect();
-
-  await expect(client.waitForPeerClosure()).resolves.toEqual({
-    code: 4000,
-    reason: 'Gateway is not yet registered',
+  await client.use(async () => {
+    await expect(client.waitForPeerClosure()).resolves.toEqual({
+      code: 4000,
+      reason: 'Gateway is not yet registered',
+    });
   });
+
   expect(mockLogs).toContainEqual(
     partialPinoLog('warn', 'Aborting courier sync because gateway is unregistered'),
   );
@@ -73,12 +72,13 @@ test('Connection should be closed with 4001 when disconnected from the courier',
   const server = makeCourierSyncServer(AUTH_TOKEN);
   const client = new MockAuthClient(server, AUTH_TOKEN);
 
-  await client.connect();
-
-  await expect(client.waitForPeerClosure()).resolves.toEqual({
-    code: 4001,
-    reason: 'Device is not connected to a courier',
+  await client.use(async () => {
+    await expect(client.waitForPeerClosure()).resolves.toEqual({
+      code: 4001,
+      reason: 'Device is not connected to a courier',
+    });
   });
+
   expect(mockLogs).toContainEqual(
     partialPinoLog('warn', 'Aborting courier sync because device is not connected to a courier'),
   );
@@ -92,12 +92,13 @@ test('Connection should be closed with 1011 when an unexpected error occurs', as
   const server = makeCourierSyncServer(AUTH_TOKEN);
   const client = new MockAuthClient(server, AUTH_TOKEN);
 
-  await client.connect();
-
-  await expect(client.waitForPeerClosure()).resolves.toEqual({
-    code: 1011,
-    reason: 'Internal server error',
+  await client.use(async () => {
+    await expect(client.waitForPeerClosure()).resolves.toEqual({
+      code: 1011,
+      reason: 'Internal server error',
+    });
   });
+
   expect(mockLogs).toContainEqual(
     partialPinoLog('error', 'Unexpected error when syncing with courier', {
       err: expect.objectContaining({
@@ -112,9 +113,9 @@ test('Authentication should be required', async () => {
   const server = makeCourierSyncServer(AUTH_TOKEN);
   const client = new MockClient(server);
 
-  await client.connect();
-
-  await expect(client.waitForPeerClosure()).resolves.toHaveProperty('code', 1008);
+  await client.use(async () => {
+    await expect(client.waitForPeerClosure()).resolves.toHaveProperty('code', 1008);
+  });
 });
 
 test('CORS should be allowed', async () => {
@@ -122,7 +123,7 @@ test('CORS should be allowed', async () => {
   const server = makeCourierSyncServer(AUTH_TOKEN);
   const client = new MockAuthClient(server, AUTH_TOKEN, { origin: 'https://example.com' });
 
-  await client.connect();
-
-  await expect(client.waitForPeerClosure()).resolves.toEqual({ code: 1000 });
+  await client.use(async () => {
+    await expect(client.waitForPeerClosure()).resolves.toEqual({ code: 1000 });
+  });
 });
--- packages/daemon/src/server/websocket.ts
+++ packages/daemon/src/server/websocket.ts
@@ -14,26 +14,35 @@ export enum WebSocketCode {
   SERVER_ERROR = 1011,
 }
 
+export interface WebSocketServerOptions {
+  readonly authToken: string;
+  readonly pingFrequencySeconds: number;
+}
+
 export function makeWebSocketServer(
   handler: (
     connectionStream: Duplex,
     socket: WebSocket,
     headers: IncomingHttpHeaders,
   ) => Promise<void>,
-  authToken?: string,
+  options: Partial<WebSocketServerOptions> = {},
 ): WebSocket.Server {
   const wsServer = new WebSocket.Server({
     clientTracking: false,
     noServer: true,
   });
 
-  const isAuthRequired = !!authToken;
+  const isAuthRequired = !!options.authToken;
   const logger = Container.get(LOGGER);
 
   wsServer.on('connection', async (socket, request) => {
+    socket.once('close', (code, reason) => {
+      logger.debug({ code, reason }, 'Closing connection');
+    });
+
     const url = new URL(request.url!!, 'http://127.0.0.1');
     const requestToken = url.searchParams.get('auth');
-    if (isAuthRequired && authToken !== requestToken) {
+    if (isAuthRequired && options.authToken !== requestToken) {
       logger.info('Refusing unauthenticated request');
       socket.close(WebSocketCode.VIOLATED_POLICY, 'Authentication is required');
       return;
@@ -43,6 +52,16 @@ export function makeWebSocketServer(
       logger.info('Refusing CORS request');
       socket.close(WebSocketCode.VIOLATED_POLICY, 'CORS requests are disabled');
       return;
+    }
+
+    if (options.pingFrequencySeconds) {
+      const pingIntervalId = setInterval(() => {
+        logger.trace('Sending ping to client');
+        socket.ping();
+      }, options.pingFrequencySeconds * 1_000);
+      socket.once('close', () => {
+        clearInterval(pingIntervalId);
+      });
     }
 
     const stream = createWebSocketStream(socket);
