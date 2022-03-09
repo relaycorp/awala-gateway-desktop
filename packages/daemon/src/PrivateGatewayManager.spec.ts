c71be8b85c55c1042b685c99e7e27f456ab511ef
Gus Narea <gnarea@users.noreply.github.com>
2022-03-09 14:59:33 +0000

    complete PrivateGatewayManager

--- packages/daemon/src/PrivateGatewayManager.spec.ts
+++ packages/daemon/src/PrivateGatewayManager.spec.ts
@@ -1,13 +1,81 @@
-import { GatewayManager } from '@relaycorp/relaynet-core';
 import { Container } from 'typedi';
 
 import { PrivateGatewayManager } from './PrivateGatewayManager';
 import { setUpTestDBConnection } from './testUtils/db';
+import { Config, ConfigKey } from './Config';
+import { MissingGatewayError } from './errors';
 
 setUpTestDBConnection();
 
-test('Class should be registered as a service', () => {
-  const gatewayManager = Container.get(PrivateGatewayManager);
+describe('createCurrentIfMissing', () => {
+  test('Node should be created if private address is absent', async () => {
+    const config = Container.get(Config);
+    await expect(config.get(ConfigKey.CURRENT_PRIVATE_ADDRESS)).resolves.toBeNull();
+    const gatewayManager = Container.get(PrivateGatewayManager);
 
-  expect(gatewayManager).toBeInstanceOf(GatewayManager);
+    await gatewayManager.createCurrentIfMissing();
+
+    const privateAddress = await config.get(ConfigKey.CURRENT_PRIVATE_ADDRESS);
+    expect(privateAddress).toBeTruthy();
+    await expect(gatewayManager.get(privateAddress!)).resolves.not.toBeNull();
+  });
+
+  test('Node should be created if private key does not', async () => {
+    const config = Container.get(Config);
+    await config.set(ConfigKey.CURRENT_PRIVATE_ADDRESS, '0deadbeef');
+    const gatewayManager = Container.get(PrivateGatewayManager);
+
+    await gatewayManager.createCurrentIfMissing();
+
+    const privateAddress = await config.get(ConfigKey.CURRENT_PRIVATE_ADDRESS);
+    await expect(gatewayManager.get(privateAddress!)).resolves.not.toBeNull();
+  });
+
+  test('Node should be reused if it already exists', async () => {
+    const gatewayManager = Container.get(PrivateGatewayManager);
+    await gatewayManager.createCurrentIfMissing();
+    const config = Container.get(Config);
+    const originalPrivateAddress = await config.get(ConfigKey.CURRENT_PRIVATE_ADDRESS);
+
+    await gatewayManager.createCurrentIfMissing();
+
+    const newPrivateAddress = await config.get(ConfigKey.CURRENT_PRIVATE_ADDRESS);
+    expect(newPrivateAddress).toBeTruthy();
+    expect(newPrivateAddress).toEqual(originalPrivateAddress);
+  });
+});
+
+describe('getCurrent', () => {
+  test('Error should be thrown if private address is absent', async () => {
+    const config = Container.get(Config);
+    await expect(config.get(ConfigKey.CURRENT_PRIVATE_ADDRESS)).resolves.toBeNull();
+    const gatewayManager = Container.get(PrivateGatewayManager);
+
+    await expect(gatewayManager.getCurrent()).rejects.toThrowWithMessage(
+      MissingGatewayError,
+      'Config does not contain current private address',
+    );
+  });
+
+  test('Error should be thrown if private key is absent', async () => {
+    const privateAddress = '0deadbeef';
+    await Container.get(Config).set(ConfigKey.CURRENT_PRIVATE_ADDRESS, privateAddress);
+    const gatewayManager = Container.get(PrivateGatewayManager);
+
+    await expect(gatewayManager.getCurrent()).rejects.toThrowWithMessage(
+      MissingGatewayError,
+      `Private key (${privateAddress}) is missing`,
+    );
+  });
+
+  test('Existing gateway should be returned', async () => {
+    const gatewayManager = Container.get(PrivateGatewayManager);
+    await gatewayManager.createCurrentIfMissing();
+
+    const gateway = await gatewayManager.getCurrent();
+
+    await expect(Container.get(Config).get(ConfigKey.CURRENT_PRIVATE_ADDRESS)).resolves.toEqual(
+      gateway.privateAddress,
+    );
+  });
 });
--- packages/daemon/src/PrivateGatewayManager.ts
+++ packages/daemon/src/PrivateGatewayManager.ts
@@ -1,15 +1,55 @@
-import { GatewayManager } from '@relaycorp/relaynet-core';
+import {
+  generateRSAKeyPair,
+  getPrivateAddressFromIdentityKey,
+  PrivateGateway,
+  PrivateGatewayManager as BasePrivateGatewayManager,
+} from '@relaycorp/relaynet-core';
 import { Inject, Service } from 'typedi';
 
 import { DBPrivateKeyStore } from './keystores/DBPrivateKeyStore';
 import { DBPublicKeyStore } from './keystores/DBPublicKeyStore';
+import { DBCertificateStore } from './keystores/DBCertificateStore';
+import { Config, ConfigKey } from './Config';
+import { MissingGatewayError } from './errors';
 
 @Service()
-export class PrivateGatewayManager extends GatewayManager {
+export class PrivateGatewayManager extends BasePrivateGatewayManager {
   constructor(
-    @Inject() protected privateKeyStore: DBPrivateKeyStore,
-    @Inject() protected publicKeyStore: DBPublicKeyStore,
+    @Inject() privateKeyStore: DBPrivateKeyStore,
+    @Inject() publicKeyStore: DBPublicKeyStore,
+    @Inject() certificateStore: DBCertificateStore,
+    @Inject() protected readonly config: Config,
   ) {
-    super(privateKeyStore, publicKeyStore);
+    super({ certificateStore, privateKeyStore, publicKeyStore });
+  }
+
+  public async getCurrent(): Promise<PrivateGateway> {
+    const privateAddress = await this.config.get(ConfigKey.CURRENT_PRIVATE_ADDRESS);
+    if (!privateAddress) {
+      throw new MissingGatewayError('Config does not contain current private address');
+    }
+    const existingGateway = await this.get(privateAddress);
+    if (!existingGateway) {
+      throw new MissingGatewayError(`Private key (${privateAddress}) is missing`);
+    }
+    return existingGateway;
+  }
+
+  public async createCurrentIfMissing(): Promise<void> {
+    const currentNodePrivateAddress = await this.config.get(ConfigKey.CURRENT_PRIVATE_ADDRESS);
+
+    if (currentNodePrivateAddress) {
+      const privateKey = await this.keyStores.privateKeyStore.retrieveIdentityKey(
+        currentNodePrivateAddress,
+      );
+      if (privateKey) {
+        return;
+      }
+    }
+
+    const keyPair = await generateRSAKeyPair();
+    await this.keyStores.privateKeyStore.saveIdentityKey(keyPair.privateKey!);
+    const privateAddress = await getPrivateAddressFromIdentityKey(keyPair.publicKey!);
+    await this.config.set(ConfigKey.CURRENT_PRIVATE_ADDRESS, privateAddress);
   }
 }
