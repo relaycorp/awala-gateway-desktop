44c6923de549db2537ff2a0aab07704d760de141
Gus Narea <gnarea@users.noreply.github.com>
2022-03-10 18:51:26 +0000

    finish keystores

--- packages/daemon/src/keystores/DBCertificateStore.spec.ts
+++ packages/daemon/src/keystores/DBCertificateStore.spec.ts
@@ -1,6 +1,9 @@
 import { Container } from 'typedi';
 
 import { DBCertificateStore } from './DBCertificateStore';
+import { setUpTestDBConnection } from '../testUtils/db';
+
+setUpTestDBConnection();
 
 test('Dependency injection should be configured properly', () => {
   Container.get(DBCertificateStore);
