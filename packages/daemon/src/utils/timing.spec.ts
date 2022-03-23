7e14b0d64eeb8f4d14e2fedddb8c3c516ad7b172
Gus Narea <gnarea@users.noreply.github.com>
2022-03-23 17:32:36 +0000

    implement sleepUntilDate()

--- packages/daemon/src/utils/timing.spec.ts
+++ packages/daemon/src/utils/timing.spec.ts
@@ -1,5 +1,13 @@
+import {
+  addMinutes,
+  addSeconds,
+  differenceInMilliseconds,
+  minutesToMilliseconds,
+  subMilliseconds,
+} from 'date-fns';
+
 import { useFakeTimers } from '../testUtils/jest';
-import { sleepSeconds } from './timing';
+import { sleepSeconds, sleepUntilDate } from './timing';
 
 useFakeTimers();
 
@@ -21,5 +29,48 @@ describe('sleepSeconds', () => {
 
     jest.advanceTimersByTime(TIMEOUT_SECONDS * 1_000);
     await sleepSecondsPromise;
+  });
+});
+
+describe('sleepUntilDate', () => {
+  const TEN_MINUTES_IN_MS = minutesToMilliseconds(10);
+
+  test('Promise should return immediately if date is in the past', async () => {
+    jest.useRealTimers();
+    const startDate = new Date();
+    const timeoutDate = subMilliseconds(startDate, 1);
+
+    await sleepUntilDate(timeoutDate);
+
+    expect(new Date()).toBeBefore(addSeconds(startDate, 3));
+  });
+
+  test('Promise should return immediately if date is current', async () => {
+    jest.useRealTimers();
+    const timeoutDate = new Date();
+
+    await sleepUntilDate(timeoutDate);
+
+    expect(new Date()).toBeBefore(addSeconds(timeoutDate, 3));
+  });
+
+  test('Promise should not resolve when date is still in the future', async () => {
+    const timeoutDate = addMinutes(new Date(), 15);
+    let promiseResolved = false;
+
+    sleepUntilDate(timeoutDate).then(() => (promiseResolved = true));
+
+    jest.advanceTimersByTime(differenceInMilliseconds(timeoutDate, new Date()) - 1);
+
+    expect(promiseResolved).toBeFalse();
+  });
+
+  test('Promise should resolve when date is reached', async () => {
+    const timeoutDate = addMinutes(new Date(), 9);
+
+    const sleepPromise = sleepUntilDate(timeoutDate);
+
+    jest.advanceTimersByTime(TEN_MINUTES_IN_MS);
+    await sleepPromise;
   });
 });
